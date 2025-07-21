4257fd8c9f3000d22d48ad225c56edc3023b229e
ARYAN <aryandhole75@gmail.com>
2025-07-21 17:17:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("getJokeBtn")
+const JokeE1 = document.getElementById("joke")
+
+btn.addEventListener("click", async () => {
+    JokeE1.innerHTML = "loading..."
+
+    const res = await fetch("https://official-joke-api.appspot.com/jokes/random");
+    const data = await res.json()
+
+    JokeE1.innerHTML = `${data.setup}<br><strong>${data.punchline}</strong>`
+})
+
+
